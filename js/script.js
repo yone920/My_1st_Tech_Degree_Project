990c3c78557dabe463f408df149a022443806774
Yonas Tekeste <yone920@gmail.com>
2019-02-26 09:33:10 -0500

    A shuffle function is added so that the qoutes would loop with out reputation.

--- js/script.js
+++ js/script.js
@@ -4,10 +4,7 @@ FSJS project 1 - A Random Quote Generator
 ******************************************/
 
 
-
-
 // This array of objects contain the quotes 
-
 let quotes = [
     {
         qoute: 'There is only one way to avoid criticism: do nothing, say nothing, and be nothing.',
@@ -44,54 +41,26 @@ let quotes = [
 ]
 
 
-
-
-
-
-
-// This function will generate random numbers between 1 and 5, and use the random swquece to pull one quote from the quotes array at a time whenever the page is refreshed.
-
-/*
-function getRandomQuote(){
-    
-    var shuffle = [];
-    for(let i = 0;i<quotes.length ; i++){
-        let random_quote = quotes[Math.floor(Math.random() * quotes.length)];
-        if(shuffle.indexOf(random_quote) == -1){
-            shuffle.push(random_quote);
+// This function will generate random numbers between 1 and 5, and use the random sequece to pull one quote from the quotes array at a time whenever the page is refreshed. It will also shuffle the qoutes so that no qoute is repeated before initiating another new loop. (I have received help from 'Jack Bashford' in stack overflow to be able to shuffle the quote with out reputation.)
+var counter = 0;
+function getRandomQuote() {
+    counter++;
+    if (counter % 5 == 0) {
+        function shuffle(array) {
+            var currentIndex = array.length, temporaryValue, randomIndex;
+            while (0 !== currentIndex) {
+                randomIndex = Math.floor(Math.random() * currentIndex);
+                currentIndex -= 1;
+                temporaryValue = array[currentIndex];
+                array[currentIndex] = array[randomIndex];
+                array[randomIndex] = temporaryValue;
+            }
+            return array;
         }
-        else
-         i--;
+        shuffle(quotes);
     }
-    
+    return quotes[counter % 5];
 }
-
-getRandomQuote();
-*/
-
-/*
-function getRandomQuote(){
-    
-    var shuffle = [];
-    for(let i = 0;i<quotes.length ; i++){
-        let random_quote = quotes[Math.floor(Math.random() * quotes.length)];
-        if(shuffle.indexOf(random_quote) == -1){
-            shuffle.push(random_quote);
-        }
-        else
-         i--;
-    }
-
-}
-*/
-
-function getRandomQuote( ) {
-        let random_quote = quotes[Math.floor(Math.random() * quotes.length)];
-        return random_quote;
-}
-
-
-
 
 
 // This function will print the random quotes on the page evetytime the page is refreshed
@@ -118,8 +87,6 @@ function printQuote() {
 }
    
 
-
-
 // This function will change the backroung color eveytime the page is refreshed or 'show another quote' buttom is clicked
 function backgroundColor() {
     var Color;
@@ -136,15 +103,12 @@ function backgroundColor() {
 backgroundColor();
 
 
-
 // This will apply time interval between qoutes and color change
 var intervalID = window.setInterval(printQuote, 5000);
 var intervalID = window.setInterval(backgroundColor, 5000);
 
 
-
 // Whenever the show another quote is clicked the event listner will call the printQuote to chnaged the coat and backgroundColor to randomly chage the background color 
-
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 document.getElementById('loadQuote').addEventListener("click", backgroundColor, false);
 
